fcbd64c063a2e8f6dbd73912761827d2afaa21f8
chanyou0311 <chanyou0311@gmail.com>
2019-09-21 20:13:56 +0900

    [add] admin追加

--- kodomo/admin.py
+++ kodomo/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from . import models
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Action)
+admin.site.register(models.Log)
+admin.site.register(models.Bank)
+admin.site.register(models.Ticket)
+admin.site.register(models.Color)
+admin.site.register(models.Price)
