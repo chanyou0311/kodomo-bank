8951dc1ed594c2d07ab71939be6a0e9679e937a7
chanyou0311 <chanyou0311@gmail.com>
2019-09-22 10:50:19 +0900

    [add] とりあえずTicket List

--- kodomo/urls.py
+++ kodomo/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = "kodomo"
 urlpatterns = [
     path("", views.HomeTemplateView.as_view(), name="home"),
+    path("tickets/", views.TicketListView.as_view(), name="tickets_list"),
 ]
+
--- kodomo/views.py
+++ kodomo/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Ticket
+
 
 class HomeTemplateView(TemplateView):
     template_name = "kodomo/home.html"
+
+
+class TicketListView(ListView):
+    template_name = "kodomo/tickets/list.html"
+    model = Ticket
+
+
+# class UseTicketView(TemplateView):
+#     template_name = "kodomo/tickets/use.html"
+
+
+# class BuyTicketView(TemplateView):
+#     template_name = "kodomo/tickets/by.html"
+
+
+# class BankView(Detail):
+#     template_name = "kodomo/banks/detail.html"
+
