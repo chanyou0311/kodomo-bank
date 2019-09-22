0663b3ea56d48c55d6b6d77e23fdd0b513e1ca95
chanyou0311 <chanyou0311@gmail.com>
2019-09-22 13:24:10 +0900

    [add] bank detail

--- kodomo/urls.py
+++ kodomo/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("tickets/buy/", views.BuyTicketView.as_view(), name="tickets_buy"),
     path("tickets/buy-ask/<int:price>", views.BuyAskTicketView.as_view(), name="tickets_buy_ask"),
     path("banks/<int:pk>/balance/", views.bank_balance_api, name="bank_balance_api"),
+    path("banks/<int:pk>/", views.BankDetailView.as_view(), name="banks_detail"),
 ]
--- kodomo/views.py
+++ kodomo/views.py
@@ -45,6 +45,11 @@ class BuyAskTicketView(TemplateView):
     template_name = "kodomo/tickets/buy_ask.html"
 
 
+class BankDetailView(DetailView):
+    template_name = "kodomo/banks/detail.html"
+    model = Bank
+
+
 def bank_balance_api(request, pk):
     bank = Bank.objects.get(pk=pk)
     d = {"balance": bank.balance}
