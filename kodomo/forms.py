d6efe27f199b4e99356d1d4f889bf3b8b224eede
chanyou0311 <chanyou0311@gmail.com>
2019-09-22 13:11:54 +0900

    [add] 残高が減る

--- kodomo/forms.py
+++ kodomo/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
 
-class AnalyticsForm(forms.Form):
-    price = forms.IntegerField(label="csvファイル")
-    column = forms.CharField(label="解析するカラム名", widget=forms.TextInput())
+class QRCodeForm(forms.Form):
+    image = forms.FileField(label="QRコード", widget=forms.FileInput())
--- kodomo/views.py
+++ kodomo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, FormView
 from django.http.response import JsonResponse
+from django.urls import reverse_lazy
 
 from .models import Ticket, Bank
+from .forms import QRCodeForm
 
 
 class HomeTemplateView(TemplateView):
@@ -14,8 +16,18 @@ class TicketListView(ListView):
     model = Ticket
 
 
-class UseTicketView(TemplateView):
+class UseTicketView(FormView):
     template_name = "kodomo/tickets/use.html"
+    form_class = QRCodeForm
+    success_url = reverse_lazy('kodomo:tickets_use')
+
+    def form_valid(self, form):
+        print("hogehoge")
+        bank = Bank.objects.get(pk=1)
+        bank.balance = bank.balance - 10
+        bank.save()
+        return super().form_valid(form)
+
 
 class UseDoneTicketView(TemplateView):
     template_name = "kodomo/tickets/use_done.html"
@@ -23,6 +35,10 @@ class UseDoneTicketView(TemplateView):
 
 class BuyTicketView(TemplateView):
     template_name = "kodomo/tickets/buy.html"
+
+    def post(self, request, *args, **kwargs):
+        self.request["POST"]
+        return render(request, self.template_name)
 
 
 class BuyAskTicketView(TemplateView):
